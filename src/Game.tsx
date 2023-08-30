47a4a99ad40497b2f24b83d78ec5881f0545ed85
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2023-08-29 20:10:45 -0500

    feat: Add Code and List to "Game" Component
    - Install the react-syntax-highlighter lib.
    - Use this lib in the "Game" Component.
    - Add the List of  possible answers
    
    #20230829b

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,13 +1,27 @@
-import { /*IconButton, Stack,*/ Card, Typography } from '@mui/material';
+import { /*IconButton, Stack,*/ Card, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 import { useQuestionsStore } from "./store/questions";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Question } from "./types";
 
 const QuestionComponent = ({ info }: { info: Question }) => {
   return (
-    <Card variant="outlined">
-      <Typography variant="h5">
-        { info.question}
+    <Card variant="outlined" sx={{ bgcolor: '#222', p: 2, textAlign: 'left' }}>
+      <Typography variant="h5" color="#fefe">
+        {info.question}
       </Typography>
+      <SyntaxHighlighter language="JavaScript" style={gradientDark}>
+        {info.code}
+      </SyntaxHighlighter>
+      <List sx={{ bgcolor: '#333', color:'#efe' }} disablePadding> 
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton>
+              <ListItemText primary={answer} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   );
 }
